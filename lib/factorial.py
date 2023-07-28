53eb7be1ff7565222838d4a9f4a41e9202e08f05
elliepriestley <ellie.priestley@gmail.com>
2023-07-28 12:00:04 +0100

    Began Intermezzo: Debugging 2

---
+++ lib/factorial.py
@@ -0,0 +1,14 @@
+# File: lib/factorial.py
+
+#? Debugging with an Interactive Debugger - exercise
+
+def factorial(n):
+    product = 1
+    while n > 0:
+        product *= n
+        n -= 1
+    return product
+
+print(factorial(5))
+# Expected: 120 (the result of: 5 * 4 * 3 * 2 * 1)
+# Actual: 24
