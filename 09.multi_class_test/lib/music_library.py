a07efa2b73effd1d7e6b4edbb65d373280ebf2bb
elliepriestley <ellie.priestley@gmail.com>
2023-07-28 15:36:19 +0100

    Finished Demonstration of MusicLibrary Integrated and Unit Tests

--- 09.multi_class_test/lib/music_library.py
+++ 09.multi_class_test/lib/music_library.py
@@ -1,22 +1,21 @@
 # File: lib/music_library.py
 
 class MusicLibrary:
-    # Public properties:
-    #   tracks: a list of strings representing tracks
-
     def __init__(self):
-        pass
+        self.tracks = []
 
     def add(self, track):
-        # Parameters:
-        #   track: an instance of Track
-        # Returns:
-        #   Nothing
-        pass
+        self.tracks.append(track)
+
+    def all(self):
+        return self.tracks
     
     def search_by_title(self, keyword):
-        # Parameters:
-        #   keyword: a string
-        # Returns:
-        #   a list of Track instances with titles that include the keyword
-        pass
+        return [
+            track 
+            for track in self.tracks 
+            if keyword in track.title
+            ]
+    
+    
+    
---
+++ 09.multi_class_test/lib/track.py
@@ -0,0 +1,13 @@
+# File: lib/track.py
+
+class Track:
+    # Public properties:
+    #   title: a string
+    #   artist: a string
+
+    def __init__(self, title, artist):
+        self.title = title
+        self.artist = artist
+
+    def format(self):
+        return f"{self.title} by {self.artist}"
