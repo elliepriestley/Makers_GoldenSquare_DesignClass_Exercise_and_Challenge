21a12720fadba0b1597100204f9df36abc50cff4
elliepriestley <ellie.priestley@gmail.com>
2023-07-31 15:22:52 +0100

    Finished Unit Tests for ToDo Class

--- 09.multi_class_test/lib/todo.py
+++ 09.multi_class_test/lib/todo.py
@@ -4,6 +4,8 @@ class Todo:
     # Public Properties:
     #   task: a string representing the task to be done
     #   complete: a boolean representing whether the task is complete
+    task = []
+    complete = None
 
     def __init__(self, task):
         # Parameters:
@@ -11,11 +13,12 @@ class Todo:
         # Side-effects:
         #   Sets the task property
         #   Sets the complete property to False
-        pass
+        self.task = task
+        self.complete = False
 
     def mark_complete(self):
         # Returns:
         #   Nothing
         # Side-effects:
         #   Sets the complete property to True
-        pass
+        self.complete = True
--- 09.multi_class_test/tests/test_todo.py
+++ 09.multi_class_test/tests/test_todo.py
@@ -1,1 +1,31 @@
 from lib.todo import Todo
+import pytest
+
+"""
+Test that we can create an instance of the ToDo class and then access the task variable
+"""
+
+def test_initalizing_class_then_accessing_task_variable():
+    todo1 = Todo("Walk the dog")
+    assert todo1.task == "Walk the dog"
+
+
+
+"""
+Test that initializing the class sets the complete status to false
+"""
+
+def test_initializing_class_sets_complete_variable_to_false():
+    todo1 = Todo("Walk the dog")
+    assert todo1.complete == False
+
+
+"""
+Test that calling the mark_complete function sets that instance complete variable to true
+"""
+
+def test_mark_complete_sets_complete_status_to_true():
+    todo1 = Todo("Walk the dog")
+    todo1.mark_complete()
+    assert todo1.complete == True
+    
